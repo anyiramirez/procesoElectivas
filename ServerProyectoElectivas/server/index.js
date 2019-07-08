2ca2bc75ef1962f9ac1c803782fc9a7b6fa80b83
Juan Minu <arcangeljuan1@unicauca.edu.co>
2019-07-08 16:28:00 -0500

    agregando codigo para iniciar sesion con Google

--- ServerProyectoElectivas/server/index.js
+++ ServerProyectoElectivas/server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const morgan = require('morgan');
 const cors = require('cors');
-const fs = require('fs');
-const readline = require('readline');
 const {google} = require('googleapis');
 
 
@@ -76,6 +74,63 @@ app.get('/asigElect', function(req, res){
     });
     
 });
+
+//--------------------------------------------------------
+const googleConfig  = new google.auth.OAuth2{
+    clientId: "99185129164-nqe1vdpu6qka0qidomi0qp7efs78epa9.apps.googleusercontent.com",
+    clientSecret: "xi_-iQI1JRZ80G2dZgppMyX0",
+    redirect: "http://localhost:4200/Administrador",
+    };
+  
+  // generate a url that asks permissions for Blogger and Google Calendar scopes
+  const defaultScope  = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/userinfo.email',
+  ];
+  
+  function createConnection() {
+    return new google.auth.OAuth2(
+        googleConfig.clientId,
+        googleConfig.clientSecret,
+        googleConfig.redirect
+    );
+  }
+  
+  function getConnectionUrl(auth) {
+    return auth.generateAuthUrl({
+      access_type: 'offline',
+      prompt: 'consent',
+      scope: defaultScope
+    });
+  }
+  
+  function getGooglePlusApi(auth) {
+    return google.plus({ version: 'v1', auth });
+  }
+  function urlGoogle() {
+    const auth = createConnection();
+    const url = getConnectionUrl(auth);
+    return url;
+  }
+  
+  
+  function getGoogleAccountFromCode(code) {
+    const data = await auth.getToken(code);
+    const tokens = data.tokens;
+    const auth = createConnection();
+    auth.setCredentials(tokens);
+    const plus = getGooglePlusApi(auth);
+    const me = await plus.people.get({ userId: 'me' });
+    const userGoogleId = me.data.id;
+    const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
+    return {
+      id: userGoogleId,
+      email: userGoogleEmail,
+      tokens: tokens,
+    };
+  }
+
+//--------------------------------------------------------
 
 // Starting server
 app.listen(app.get('port'), () => {
