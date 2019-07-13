e5131d9fd2a6a203f17c2e306c288b174c8d5cbd
anyiramirez <anyiramirez@unicauca.edu.co>
2019-07-13 03:51:13 -0500

    inscripción registro

--- AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
+++ AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
@@ -14,9 +14,36 @@ export class InscripcionComponent implements OnInit {
   
   inscripcion:any={};
   electivasRegistradas = new Array();
- 
+  electivasDos = new Array();
+  electivasTres= new Array();
   constructor(private registrar:RegistroDatosService,private router:Router) {
     this.listarElectivas();
+    this.registrar.obtenerElectivasOfertadas("PIS").subscribe(res => {
+      this.electivasRegistradas=new Array();
+      //this.registrar.electivas= res as Electivas[];
+      for(let p in res){
+        debugger;
+        this.electivasRegistradas.push(res[p]);
+        debugger;
+      }
+      
+    }
+    );
+
+    for(let i in this.electivasRegistradas){
+      if(this.electivasRegistradas[i]!=this.inscripcion.opcion1){
+        debugger;
+        this.electivasDos.push(this.electivasRegistradas[i]);
+
+      }
+    }
+    for(let i in this.electivasDos){
+      if(this.electivasDos[i]!=this.inscripcion.opcion2){
+        debugger;
+        this.electivasTres.push(this.electivasDos[i]);
+
+      }
+    }
    }
 
   ngOnInit() {
@@ -27,11 +54,28 @@ export class InscripcionComponent implements OnInit {
       this.electivasRegistradas=new Array();
       //this.registrar.electivas= res as Electivas[];
       for(let p in res){
+        debugger;
         this.electivasRegistradas.push(res[p]);
+        debugger;
       }
       
     }
     );
+
+    for(let i in this.electivasRegistradas){
+      if(this.electivasRegistradas[i]!=this.inscripcion.opcion1){
+        debugger;
+        this.electivasDos.push(this.electivasRegistradas[i]);
+
+      }
+    }
+    for(let e in this.electivasDos){
+      if(this.electivasDos[e]!=this.inscripcion.opcion2){
+        debugger;
+        this.electivasTres.push(this.electivasDos[e]);
+      }
+    
+    }
 
   }
 
@@ -45,6 +89,13 @@ export class InscripcionComponent implements OnInit {
       
     }
     );
+
+    for(let i in this.electivasRegistradas){
+      if(this.electivasRegistradas[i]!=this.inscripcion.opcion1){
+        this.electivasDos.push(this.electivasRegistradas[i]);
+
+      }
+    }
     
   }
 }
