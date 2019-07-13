6a8bdef67f75951e13156a4102f94ac217725a5c
anyiramirez <anyiramirez@unicauca.edu.co>
2019-07-13 00:21:55 -0500

    inscripcion

--- AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
+++ AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Electivas} from '../../Interfaces/electivas';
+import { RegistroDatosService} from '../../Services/registro-datos.service';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscripcion',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscripcion.component.css']
 })
 export class InscripcionComponent implements OnInit {
-
-  constructor() { }
+  inscripcion:any={};
+  electivasRegistradas = new Array();
+  constructor(private registrar:RegistroDatosService,private router:Router) {
+    this.listarElectivas();
+   }
 
   ngOnInit() {
   }
+  registrarInscripcion(){
 
+  }
+  listarElectivas(){
+    this.registrar.obtenerInformacionElectivas().subscribe(res => {
+      this.electivasRegistradas=new Array();
+      //this.registrar.electivas= res as Electivas[];
+      for(let p in res){
+        this.electivasRegistradas.push(res[p]);
+      }
+      
+    }
+    );
+    
+  }
 }
