177a88a913b1530106f4cc68ee0316f5b63b8a4b
Valentina Vera <valentinav@unicauca.edu.co>
2019-07-27 15:14:21 -0500

    muestra 6 opciones para pis, 4 para los demás

--- AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
+++ AppProyectoElectivas/src/app/Components/inscripcion/inscripcion.component.ts
@@ -33,13 +33,14 @@ export class InscripcionComponent implements OnInit {
   codigoFormControl;
   programaFormControl;
   opcion1FormControl;
+
+  opcionesElec: boolean;
   
   habilitarop2: boolean = true;
   habilitarop3: boolean = true;
   habilitarop4: boolean = true;
   habilitarop5: boolean = true;
   habilitarop6: boolean = true;
-  opcionesElec: boolean = true;
 
   texto: any;
   nuevoTexto: any;
@@ -186,17 +187,13 @@ export class InscripcionComponent implements OnInit {
       }
     }
   }
- 
-  mostrarOpciones(){
-    debugger;
-    if (this.inscripcion.programa === 'PIS'){
-      this.opcionesElec = false;
-    }
-    if (this.inscripcion.programa === 'PIAI'||this.inscripcion.programa === 'PIET'){
+
+  mostrarOpciones(programaSeleccionado: string){
+    if(programaSeleccionado === 'PIS'){
       this.opcionesElec = true;
     }
     else{
-      this.opcionesElec = true;
+      this.opcionesElec = false;
     }
   }
 
