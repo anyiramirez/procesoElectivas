3810383c26c3988662a3db3e4d30c66a1763721e
Gustavo Morán <tavomoran@gmail.com>
2019-07-29 17:13:44 -0500

    el metodo electivasPrograma regresa la lista de las electivas por programa de acuerdo a la fecha en la que se hace la consulta

--- ServerProyectoElectivas/server/controllers/employee.controller.js
+++ ServerProyectoElectivas/server/controllers/employee.controller.js
@@ -177,14 +177,23 @@ employeeCtrl.electivasPrograma = (req, res) => {
     var nombre = String(req.params.programa);
     var list;
     var listaPrograma = [];
-    
+    fechaActual = moment().format('YYYY/MM/DD HH:mm:ss Z');
     db.ref('Ofertas').once("value", function(snapshot) {        
         list = snapshot.val();
-        //res.json(list);
-        ofertaActiva();
         var key1;
         for (key1 in list) {
-            if(list[key1].anio === '2019' && list[key1].periodo === '2'){
+            console.log("fechaActual:           " + fechaActual);
+            console.log("list[key].fechaInicio: " + list[key1].fechaInicio);
+            console.log("list[key].fechaFin:    " + list[key1].fechaFin);
+            actual = moment(fechaActual, "YYYY/MM/DD HH:mm:ss Z").toDate();
+            inicio = moment(list[key1].fechaInicio, "YYYY/MM/DD HH:mm:ss Z").toDate();
+            fin = moment(list[key1].fechaFin, "YYYY/MM/DD HH:mm:ss Z").toDate();
+            console.log("++++++++++++++++++++++++++++++");
+            console.log(actual);
+            console.log(inicio);
+            console.log(fin);
+            console.log("++++++++++++++++++++++++++++++");
+            if(inicio <= actual && actual<=fin) {
                 for (key2 in list[key1].electivasOfertadas) {
                     if(list[key1].electivasOfertadas[key2].programa.search(req.params.programa)!=-1) {
                         listaPrograma.push(list[key1].electivasOfertadas[key2].NombreElectiva);
